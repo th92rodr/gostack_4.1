4025683d2646fd2bd77f9c3bf7f263f823dfbe9f
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-24 20:43:00 -0300

    create the create-product service

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,19 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const checkProductExists = await this.productsRepository.findByName(name);
+
+    if (checkProductExists) {
+      throw new AppError('Product already exists.');
+    }
+
+    return this.productsRepository.create({ name, price, quantity });
   }
 }
 
