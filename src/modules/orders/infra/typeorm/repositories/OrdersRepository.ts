1d19fa7cea39f1c36e8a765c9420be6f0905ba11
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-25 19:40:49 -0300

    create the orders repository

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -3,6 +3,7 @@ import { getRepository, Repository } from 'typeorm';
 import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
 import ICreateOrderDTO from '@modules/orders/dtos/ICreateOrderDTO';
 import Order from '../entities/Order';
+import OrdersProducts from '../entities/OrdersProducts';
 
 class OrdersRepository implements IOrdersRepository {
   private ormRepository: Repository<Order>;
@@ -12,11 +13,36 @@ class OrdersRepository implements IOrdersRepository {
   }
 
   public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
-    // TODO
+    const order = this.ormRepository.create({ customer });
+    await this.ormRepository.save(order);
+
+    const ordersProductsList: OrdersProducts[] = [];
+    const ordersProductsRepository = getRepository(OrdersProducts);
+
+    products.forEach(product => {
+      const ordersProducts = ordersProductsRepository.create({
+        order_id: order.id,
+        product_id: product.product_id,
+        price: product.price,
+        quantity: product.quantity,
+      });
+
+      ordersProductsList.push(ordersProducts);
+    });
+
+    order.order_products = ordersProductsList;
+
+    await this.ormRepository.save(order);
+
+    await ordersProductsRepository.save(ordersProductsList);
+
+    return order;
   }
 
   public async findById(id: string): Promise<Order | undefined> {
-    // TODO
+    return this.ormRepository.findOne(id, {
+      relations: ['customer', 'order_products'],
+    });
   }
 }
 
