a50bda334e5e000510ea63a4957d037520db2d97
th92rodr <thiago.clrodrigues@gmail.com>
2020-09-25 19:42:19 -0300

    create the create-order service

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,58 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer does not exists.');
+    }
+
+    const orderProducts = [];
+    const productsEntitiesToUpdate = [];
+
+    for (const product of products) {
+      const productEntity = await this.productsRepository.findById(product.id);
+
+      if (!productEntity) {
+        throw new AppError('Product does not exists.');
+      }
+      if (productEntity.quantity - product.quantity < 0) {
+        throw new AppError('Products with insufficient quantities.');
+      }
+
+      const productEntityToUpdate = {
+        id: productEntity.id,
+        quantity: productEntity.quantity - product.quantity,
+      };
+      productsEntitiesToUpdate.push(productEntityToUpdate);
+
+      const orderProduct = {
+        product_id: productEntity.id,
+        price: productEntity.price,
+        quantity: product.quantity,
+      };
+      orderProducts.push(orderProduct);
+    }
+
+    await this.productsRepository.updateQuantity(productsEntitiesToUpdate);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return order;
   }
 }
 
